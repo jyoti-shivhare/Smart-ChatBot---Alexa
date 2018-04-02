188a1f6b4e33ca54847acb705d1873114c7d7729
Jyoti Shivhare <25609828+jyoti-shivhare@users.noreply.github.com>
2018-04-02 19:00:30 -0400

    Added variety to the responses

--- Version1.py
+++ Version1.py
@@ -23,3 +23,35 @@ def respond(message):
 # "what's today's weather?"
 # "what's your name?"
 # "what's your favorite color?"
+
+# Adding variety to the responses
+# Import the random module
+import random
+
+name = "Greg"
+weather = "cloudy"
+
+# Define a dictionary containing a list of responses for each message
+responses = {
+  "what's your name?": [
+      "my name is {0}".format(name),
+      "they call me {0}".format(name),
+      "I go by {0}".format(name)
+   ],
+  "what's today's weather?": [
+      "the weather is {0}".format(weather),
+      "it's {0} today".format(weather)
+    ],
+  "default": ["default message"]
+}
+
+# Use random.choice() to choose a matching response
+def respond(message):
+    # Check if the message is in the responses
+    if message in responses:
+        # Return a random matching response
+        bot_message = random.choice(responses[message])
+    else:
+        # Return a random "default" response
+        bot_message = random.choice(responses["default"])
+    return bot_message
