9a9744998b2354a3331c8c2e88e299b128802a91
Jyoti Shivhare <25609828+jyoti-shivhare@users.noreply.github.com>
2018-04-02 21:14:54 -0400

    Using more regex like Capitalized letters recognizing names

--- Version1.py
+++ Version1.py
@@ -146,3 +146,68 @@ send_message("what if you could be anything you wanted")
 ## Defining a "keywords" dictionary
 keywords = 
 {'goodbye': ['bye', 'farewell'], 'greet': ['hello', 'hi', 'hey'], 'thankyou': ['thank', 'thx']}
+
+# Define a dictionary of patterns
+patterns = {}
+
+# Iterate over the keywords dictionary
+for intent, keys in keywords.items():
+    # Create regular expressions and compile them into pattern objects
+    patterns[intent] = re.compile("|".join(keys))
+    
+# Print the patterns
+print(patterns)
+
+## We have patterns dictionary created, now we'll define a function to find the intent of a message
+# Define a function to find the intent of a message
+def match_intent(message):
+    matched_intent = None
+    for intent, pattern in patterns.items():
+        # Check if the pattern occurs in the message 
+        if pattern.search:
+            matched_intent = intent
+    return matched_intent
+
+# Define a respond function
+def respond(message):
+    # Call the match_intent function
+    intent = match_intent(message)
+    # Fall back to the default response
+    key = "default"
+    if intent in responses:
+        key = intent
+    return responses[key]
+
+# Send messages
+send_message("hello!")
+send_message("bye byeee")
+send_message("thanks very much!")
+
+# Define find_name()
+def find_name(message):
+    name = None
+    # Create a pattern for checking if the keywords occur
+    name_keyword = re.compile('name|call')
+    # Create a pattern for finding capitalized words
+    name_pattern = re.compile('[A-Z]{1}[a-z]*')
+    if name_keyword.search(message):
+        # Get the matching words in the string
+        name_words = name_pattern.findall(message)
+        if len(name_words) > 0:
+            # Return the name if the keywords are present
+            name = ' '.join(name_words)
+    return name
+
+# Define respond()
+def respond(message):
+    # Find the name
+    name = find_name(message)
+    if name is None:
+        return "Hi there!"
+    else:
+        return "Hello, {0}!".format(name)
+
+# Send messages
+send_message("my name is David Copperfield")
+send_message("call me Ishmael")
+send_message("People call me Cassandra")
