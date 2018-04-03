f39f79dab677b50c978efb5cc7528f44266294ad
Jyoti Shivhare <25609828+jyoti-shivhare@users.noreply.github.com>
2018-04-03 13:58:44 -0400

    Entity recogniser

--- Version1.py
+++ Version1.py
@@ -306,3 +306,23 @@ def assign_colors(doc):
 # Assign the colors
 assign_colors(doc)
 
+# Import necessary modules
+from rasa_nlu.converters import load_data
+from rasa_nlu.config import RasaNLUConfig
+from rasa_nlu.model import Trainer
+
+# Create args dictionary
+args = {"pipeline": "spacy_sklearn"}
+
+# Create a configuration and trainer
+config = RasaNLUConfig(cmdline_args = args)
+trainer = Trainer(config)
+
+# Load the training data
+training_data = load_data("./training_data.json")
+
+# Create an interpreter by training the model
+interpreter = trainer.train(training_data)
+
+# Try it out
+print(interpreter.parse("I'm looking for a Mexican restaurant in the North of town"))
