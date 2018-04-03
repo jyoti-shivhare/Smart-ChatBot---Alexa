464db10f38437bb38149692ee77a4dbc78c9ab5d
Jyoti Shivhare <25609828+jyoti-shivhare@users.noreply.github.com>
2018-04-03 01:01:41 -0400

    Update Version1.py

--- Version1.py
+++ Version1.py
@@ -211,3 +211,28 @@ def respond(message):
 send_message("my name is David Copperfield")
 send_message("call me Ishmael")
 send_message("People call me Cassandra")
+
+## Word vectors with spaCy
+# You're going to use the ATIS dataset, which contains thousands of sentences from real people interacting with a flight booking system.
+# Your job is to create a 2D array X with as many rows as there are sentences in the dataset, where each row is a vector describing that sentence
+
+import spacy
+
+# Load the spacy model: nlp
+nlp = spacy.load('en')
+
+# Calculate the length of sentences
+n_sentences = len(sentences)
+
+# Calculate the dimensionality of nlp
+embedding_dim = nlp.vocab.vectors_length
+
+# Initialize the array with zeros: X
+X = np.zeros((n_sentences, embedding_dim))
+
+# Iterate over the sentences
+for idx, sentence in enumerate(sentences):
+    # Pass each sentence to the nlp object to create a document
+    doc = nlp(sentence)
+    # Save the document's .vector attribute to the corresponding row in X
+    X[idx, :] = doc.vector
